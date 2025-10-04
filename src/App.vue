ac5933c4a1d22f81c9ec9e33894271a7c61c7a86
EveningReeze <z17690728020@163.com>
2025-10-05 01:10:43 +0300

    feat: appfor

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
 
 <script setup lang="ts">
 for (let index = 0; index < 10; index++) {
-  const wtr = 'wodehiqh'
+  const wtr = 'wodehiqh';
+  console.log(i,"123");
+  
 }
-let str ="123sad";
+let str = '123sad';
 </script>
 
 <style scoped></style>
