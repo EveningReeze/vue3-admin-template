7bf552e7d5fd50e1625cbd4345634865fc44746b
EveningReeze <z17690728020@163.com>
2025-10-05 01:01:54 +0300

    feat: appfor

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="box">
-    <h1>hello,app，https://github.com/liJie-wk/v3-ts-admin</h1>
+  <div>
+
   </div>
 </template>
+
 <script setup lang="ts">
-const fun = () => {
-  console.log('123')
-  const str = '我爱住过'
+for (let index = 0; index < 10; index++) 
+{
+const wtr = "wodehiqh"
+  
 }
-fun()
 </script>
 
-<style scoped lang="scss">
-.box {
-  width: 200px;
-  height: 200px;
-  h1 {
-    font-optical-sizing: 123;
-  }
-}
+<style scoped>
+
 </style>
